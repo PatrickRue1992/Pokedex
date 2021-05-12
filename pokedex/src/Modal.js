b13ccd42e45113779473a1dba58315eee84e96f1
PatrickRue1992 <patrick.ruehle@gmx.de>
2021-05-12 10:54:28 +0200

    added Pokemon Details Height/Weight/types

--- pokedex/src/Modal.js
+++ pokedex/src/Modal.js
@@ -22,7 +22,12 @@ function Modal({ closeModal, singlePoke }) {
             }}
           >
             <div className="id">
-              <p>#0{singlePoke.id}</p>
+              <p>
+                {" "}
+                {singlePoke.id > 99
+                  ? `#${singlePoke.id}`
+                  : `#0${singlePoke.id}`}
+              </p>
             </div>
             <img
               src={singlePoke.sprites.other["official-artwork"].front_default}
@@ -32,7 +37,35 @@ function Modal({ closeModal, singlePoke }) {
               <p>{singlePoke.name}</p>
             </div>
           </div>
-          <p>MODAL</p>
+          <div className="Biography">
+            <p>Bio</p>
+            <div className="bio_data">
+              <p>
+                Height:
+                <span>
+                  {singlePoke.height / 10}m (
+                  {Math.floor(((singlePoke.height / 10) * 39.37) / 12)}&apos;
+                  {(((singlePoke.height / 10) * 39.37) % 12).toFixed(1)}
+                  &quot;)
+                </span>
+              </p>
+              <p>
+                Weight:
+                <span>
+                  {singlePoke.weight / 10}kg (
+                  {((singlePoke.weight / 10) * 2.2).toFixed(1)} lbs)
+                </span>
+              </p>
+              <p>
+                Type:{" "}
+                <span>
+                  {singlePoke.types.length > 1
+                    ? `${singlePoke.types[0].type.name} / ${singlePoke.types[1].type.name}`
+                    : `${singlePoke.types[0].type.name}`}
+                </span>
+              </p>
+            </div>
+          </div>
           <p>NAME</p>
         </div>
       </div>
